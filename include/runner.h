daaa2174f952b41d639a473cd25b512f75a1ebdf
harooonm <47739642+harooonm@users.noreply.github.com>
2019-05-19 11:28:44 +0400

    Update runner.h

--- include/runner.h
+++ include/runner.h
@@ -2,8 +2,6 @@
 #define _RUNNER_H_
 
 #include "roc_common.h"
-
-extern void runner_init(char *cmd, char **args);
 extern void runner_run(char *s_path, char *e_path, char *tag);
 extern void runner_stop(void);
 
